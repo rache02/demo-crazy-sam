da7b590cce873e73df328c9b7d050a9073b0413b
Sam Young <samwilsonyoung@gmail.com>
2017-08-07 21:07:30 +1000

    added a space

--- app.js
+++ app.js
@@ -10,11 +10,29 @@ app.use(express.static('public'));
 
 //routes
 app.get('/', function(req,res) {
-  
+ 
   }
 )
+app.get('/users/new', function(req,res) {
 
+  }
+)
+app.post('/users', function(req,res) {
 
+  }
+)
+app.get('/dashboard', function(req,res) {
+
+  }
+)
+app.post('/users/:id/votes', function(req,res) {
+
+  }
+)
+app.delete('/users/:id/votes', function(req,res) {
+
+  }
+)
 
 
 app.listen(PORT, function() {
