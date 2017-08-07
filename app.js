303ff3563787da5e6e9032cbce61560fe2ffd9eb
Sam Young <samwilsonyoung@gmail.com>
2017-08-07 20:40:04 +1000

    added initial files

---
+++ app.js
@@ -0,0 +1,22 @@
+const PORT = 3000;
+var express = require('express');
+var app = express();
+var request = require('request');
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+//routes
+app.get('/', function(req,res) {
+  
+  }
+)
+
+
+
+
+app.listen(PORT, function() {
+  console.log(`Server now listening on port ${PORT}`);
+}
